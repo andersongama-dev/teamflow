2170649418e943557ac320e249e046db33ba9ef5
andersongama-dev <andersongamasilva5@gmail.com>
2026-06-12 22:41:33 -0300

    refactor: refactoring the sidebar style and making UI adjustments

--- resources/views/layoutApp.blade.php
+++ resources/views/layoutApp.blade.php
@@ -17,27 +17,29 @@
 </head>
 
 <body class="min-h-screen bg-white dark:bg-zinc-800 antialiased">
-
-    <flux:sidebar sticky collapsible="mobile"
-        class="bg-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-700">
+    <flux:sidebar sticky collapsible class="bg-zinc-50 dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-700">
         <flux:sidebar.header>
-            <flux:sidebar.brand href="#"
-                logo="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Microsoft_Office_Teams_%282025%E2%80%93present%29.svg/1280px-Microsoft_Office_Teams_%282025%E2%80%93present%29.svg.png"
-                logo:dark="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Microsoft_Office_Teams_%282025%E2%80%93present%29.svg/1280px-Microsoft_Office_Teams_%282025%E2%80%93present%29.svg.png"
-                name="TeamFLow" />
-            <flux:sidebar.collapse class="lg:hidden" />
+            <flux:sidebar.brand href="#" logo="https://fluxui.dev/img/demo/logo.png"
+                logo:dark="{{ asset('assets/logoTeamFlow.png') }}" name="TeamFlow" />
+            <flux:sidebar.collapse
+                class="in-data-flux-sidebar-on-desktop:not-in-data-flux-sidebar-collapsed-desktop:-mr-2" />
         </flux:sidebar.header>
-        <flux:sidebar.search placeholder="Search..." />
         <flux:sidebar.nav>
-            <flux:sidebar.item icon="home" href="/materiais" current>Materials</flux:sidebar.item>
-            <flux:sidebar.item icon="inbox" badge="12" href="#">Inbox</flux:sidebar.item>
-            <flux:sidebar.item icon="document-text" href="#">Documents</flux:sidebar.item>
-            <flux:sidebar.item icon="calendar" href="#">Calendar</flux:sidebar.item>
-            <flux:sidebar.group expandable heading="Favorites" class="grid">
+            <flux:sidebar.item icon="chart-pie" href="/dashboard">Dashboard</flux:sidebar.item>
+            <flux:sidebar.item icon="academic-cap" href="/classes">Minhas turmas</flux:sidebar.item>
+            <flux:sidebar.item icon="book-open" href="/subjects">Matérias</flux:sidebar.item>
+            <flux:sidebar.item icon="arrow-top-right-on-square" href="/notes">Lançar Notas</flux:sidebar.item>
+            <flux:sidebar.item icon="calendar-days" href="/frequency">Frequência</flux:sidebar.item>
+            <flux:sidebar.item icon="document-text" href="/reports">Relatórios</flux:sidebar.item>
+            <?php
+            /*
+            <flux:sidebar.group expandable icon="star" heading="Favorites" class="grid">
                 <flux:sidebar.item href="#">Marketing site</flux:sidebar.item>
                 <flux:sidebar.item href="#">Android app</flux:sidebar.item>
                 <flux:sidebar.item href="#">Brand guidelines</flux:sidebar.item>
             </flux:sidebar.group>
+            */
+            ?>
         </flux:sidebar.nav>
         <flux:sidebar.spacer />
         <flux:sidebar.nav>
@@ -45,11 +47,13 @@
             <flux:sidebar.item icon="information-circle" href="#">Help</flux:sidebar.item>
         </flux:sidebar.nav>
         <flux:dropdown position="top" align="start" class="max-lg:hidden">
-            <flux:sidebar.profile avatar="https://fluxui.dev/img/demo/user.png" name="Olivia Martin" />
+            <flux:sidebar.profile
+                avatar="https://4.bp.blogspot.com/-83HGO7a2KV4/U6yEBTghSeI/AAAAAAAB9Ys/dslH1eKaueY/s1600/fernandinho.jpg"
+                name="Fernandinho" />
             <flux:menu>
                 <flux:menu.radio.group>
-                    <flux:menu.radio checked>Olivia Martin</flux:menu.radio>
-                    <flux:menu.radio>Truly Delta</flux:menu.radio>
+                    <flux:menu.radio checked>Fernandinho</flux:menu.radio>
+                    <flux:menu.radio>Professor do TeamFlow</flux:menu.radio>
                 </flux:menu.radio.group>
                 <flux:menu.separator />
                 <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
@@ -59,12 +63,12 @@
     <flux:header class="lg:hidden">
         <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
         <flux:spacer />
-        <flux:dropdown position="top" alignt="start">
-            <flux:profile avatar="https://fluxui.dev/img/demo/user.png" />
+        <flux:dropdown position="top" align="start">
+            <flux:profile avatar="/img/demo/user.png" />
             <flux:menu>
                 <flux:menu.radio.group>
-                    <flux:menu.radio checked>Olivia Martin</flux:menu.radio>
-                    <flux:menu.radio>Truly Delta</flux:menu.radio>
+                    <flux:menu.radio checked>Fernandinho</flux:menu.radio>
+                    <flux:menu.radio>Professor do TeamFlow</flux:menu.radio>
                 </flux:menu.radio.group>
                 <flux:menu.separator />
                 <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
