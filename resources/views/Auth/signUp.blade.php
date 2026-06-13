4fb3e3791fa45672d4bfc3b8aa0592cc7b55de99
andersongama-dev <andersongamasilva5@gmail.com>
2026-06-13 11:09:55 -0300

    refactor: the SignUp screen has been redesigned

--- resources/views/Auth/signUp.blade.php
+++ resources/views/Auth/signUp.blade.php
@@ -2,25 +2,62 @@
 
 @section('container')
     <div class="flex justify-center items-center h-dvh">
-        <form action="{{ url('sign-up') }}" method="POST" class="w-96">
-            @csrf
-
-            <div class="flex flex-col gap-6">
-                <flux:input type="text" name="name" label="Nome" value="{{ old('name') }}" />
-                <flux:input type="email" name="email" label="E-mail" value="{{ old('email') }}" />
-                <flux:input type="password" name="password" label="Senha">
-                    <x-slot name="iconTrailing">
-                        <flux:button size="sm" variant="subtle" icon="eye" class="-mr-1" />
-                    </x-slot>
-                </flux:input>
-                <flux:input type="password" name="password_confirmation" label="Confirmar senha">
-                    <x-slot name="iconTrailing">
-                        <flux:button size="sm" variant="subtle" icon="eye" class="-mr-1" />
-                    </x-slot>
-                </flux:input>
+        <flux:card class="space-y-6 w-96">
+            <div>
+                <flux:heading size="lg">Criar conta</flux:heading>
+                <flux:text class="mt-2">
+                    Preencha os dados para se cadastrar.
+                </flux:text>
             </div>
-            <flux:button variant="primary" color="orange" class="w-full mt-8 cursor-pointer" type="submit">Registrar-se
-            </flux:button>
-        </form>
+
+            <form action="{{ url('sign-up') }}" method="POST">
+                @csrf
+
+                <div class="space-y-6">
+                    <flux:input label="Nome" type="text" name="name" value="{{ old('name') }}"
+                        placeholder="Seu nome" />
+
+                    <flux:error name="name" />
+
+                    <flux:input label="E-mail" type="email" name="email" value="{{ old('email') }}"
+                        placeholder="Seu endereço de e-mail" />
+
+
+                    <flux:field>
+                        <flux:label>Senha</flux:label>
+
+                        <flux:input type="password" name="password" placeholder="Sua senha">
+                            <x-slot name="iconTrailing">
+                                <flux:button size="sm" variant="subtle" icon="eye" class="-mr-1" type="button" />
+                            </x-slot>
+                        </flux:input>
+
+                        <flux:error name="password" />
+                    </flux:field>
+
+                    <flux:field>
+                        <flux:label>Confirmar senha</flux:label>
+
+                        <flux:input type="password" name="password_confirmation" placeholder="Confirme sua senha">
+                            <x-slot name="iconTrailing">
+                                <flux:button size="sm" variant="subtle" icon="eye" class="-mr-1" type="button" />
+                            </x-slot>
+                        </flux:input>
+                    </flux:field>
+                </div>
+
+                <div class="space-y-2 mt-6">
+                    <flux:button type="submit" variant="primary" color="orange" class="w-full">
+                        Registrar-se
+                    </flux:button>
+
+                    <a href="/sign-in" class="block">
+                        <flux:button type="button" variant="ghost" class="w-full">
+                            Já tenho uma conta
+                        </flux:button>
+                    </a>
+                </div>
+            </form>
+        </flux:card>
     </div>
 @endsection
